5875721c5b02dc7958dbf8a7dffd43e533136a06
Oleg Brezhnev <oleg-423@yandex.ru>
2018-11-13 17:19:30 +0300

    scrpper launch order changed

--- run.py
+++ run.py
@@ -16,21 +16,21 @@ scrappers = [
     BazarpnzSpider,
 ]
 
-os.environ["http_proxy"] = "http://localhost:8888"
+#os.environ["http_proxy"] = "http://localhost:8888"
 output_file = 'output.json'
+if os.path.isfile(output_file):
+    os.remove(output_file)
+settings = get_project_settings()
+process = CrawlerProcess(settings)
 for scrapper in scrappers:
-    if os.path.isfile(output_file):
-        os.remove(output_file)
-    settings = get_project_settings()
-    process = CrawlerProcess(settings)
     process.crawl(scrapper)
-    process.start()
-    with open(output_file, 'r') as f:
-        text = f.read()
-        items = json.loads(text.encode("utf8"))
-        for item in items:
-            response = requests.post(push_url, data=json.dumps({'order': item}),
-                                     headers={'Accept': 'application/json', 'Content-Type': 'application/json'})
-            print(response.content)
+process.start()
+with open(output_file, 'r') as f:
+    text = f.read()
+    items = json.loads(text.encode("utf8"))
+    for item in items:
+        response = requests.post(push_url, data=json.dumps({'order': item}),
+                                 headers={'Accept': 'application/json', 'Content-Type': 'application/json'})
+        print(response.content)
 
 
