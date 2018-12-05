ce0648cc16851d8217eb18dc55f67bf6259bc292
Oleg Brezhnev <gimemor@Air-Oleg.Dlink>
2018-12-05 12:45:43 +0300

    avito adjustment

--- avitoscrapper/spiders/avito_ru.py
+++ avitoscrapper/spiders/avito_ru.py
@@ -3,6 +3,7 @@ import scrapy
 import logging
 import traceback
 import datetime
+import requests
 import re
 from scrapy.loader import ItemLoader
 from ..items import Ad
@@ -34,16 +35,14 @@ class AvitoRuSpider(scrapy.Spider):
     date_regex = re.compile(r"размещено\s*(\d+\s*\w+|сегодня|вчера)", re.I)
     outdate_treshold = 2
     custom_settings = {
-        'ROBOTSTXT_OBEY': True,
+        'ROBOTSTXT_OBEY': False,
         'DOWNLOAD_DELAY': 1.1
     }
 
     def __init__(self):
         scrapy.Spider.__init__(self)
         self.driver_options = Options()
-        self.driver_options.headless = True
-        self.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',
-                                        options=self.driver_options)
+
         self.total_count = 0
 
     # noinspection PyMethodMayBeStatic
@@ -76,43 +75,24 @@ class AvitoRuSpider(scrapy.Spider):
     # noinspection PyMethodMayBeStatic
     def get_address(self, response):
         district = response.xpath(
-            '//span[contains(@class, \'item-map-address\')]/span/text()').extract_first().strip()
+            '//span[contains(@class, \'item-map-address\')]/span/text()').extract_first()
+        if district is None:
+            return None
         # address = response.xpath(
         #    '//span[contains(@class, \'item-map-address\')]/span/span/text()').extract_first().strip()
-        return district  # + address
+        return district.strip()  # + address
 
     # noinspection PyMethodMayBeStatic
     def get_description(self, response):
         descriptions = response.xpath('//div[contains(@class, \'item-description\')]/p/text()').extract()
         return '\r\n'.join(descriptions)
 
+    # noinspection PyMethodMayBeStatic
     def get_phone(self, response):
-        try:
-            self.driver.get(response.url)
-            wait = WebDriverWait(self.driver, 30)
-            button_xpath = "//div[contains(@class, \'item-phone-number\')]/a"
-            wait.until(
-                expected_condition.presence_of_element_located(
-                    (By.XPATH, button_xpath)))
-            button = self.driver.find_element_by_xpath(button_xpath)
-            webdriver.ActionChains(self.driver).move_to_element(button).click(button).perform()
-            img_xpath = '//div[contains(@class, \'item-phone-big-number\')]/img'
-            wait.until(
-                expected_condition.presence_of_element_located(
-                    (By.XPATH, img_xpath)))
-            img_element = self.driver.find_element_by_xpath(img_xpath)
-            raw_img_string = img_element.get_attribute("src")
-            raw_img_string = raw_img_string.split('base64,')[-1].strip()
-            with io.BytesIO() as pic,\
-                    io.BytesIO(base64.b64decode(raw_img_string)) as image_string:
-                image = Image.open(image_string)
-                image.save(pic, image.format, quality=100)
-                return pytesseract.image_to_string(image)
-        except Exception:
-            logging.error(traceback.format_exc())
-            Logger.log('Error',
-                       'Error has occurred during the phone recognition:\n{}'.format(traceback.format_exc()))
-            return None
+        phone_raw = response.xpath("//a[contains(@data-marker, 'item-contact-bar/call')]/@href").extract_first()
+        reg = re.compile('tel:([\d\+ -]+)', re.I)
+        result = reg.findall(phone_raw)
+        return result[0] if result else None
 
     # noinspection PyMethodMayBeStatic
     def get_room_count(self, response):
@@ -200,6 +180,8 @@ class AvitoRuSpider(scrapy.Spider):
 
     def get_city(self, response):
         address = self.get_address(response)
+        if address is None:
+            return 'Пенза'
         items = address.split(',')
         return items[0] if not 'р-н' in items[0] else 'Пенза'
 
@@ -211,9 +193,14 @@ class AvitoRuSpider(scrapy.Spider):
                 return i
         return 'Неизвестно'
 
+    def parse_mobile(self, response):
+        ad_loader = response.meta['ad_loader']
+        ad_loader.add_value('phone', self.get_phone(response))
+        return ad_loader.load_item()
+
     def parse_ad(self, response):
         """
-        @url https://www.avito.ru/penza/komnaty/komnata_11_m_v_2-k_29_et._976019279
+        @url https://m.avito.ru/penza/komnaty/komnata_11_m_v_2-k_29_et._976019279
         """
         ad_loader = ItemLoader(item=Ad(), response=response)
         ad_loader.add_xpath('title', '//span[contains(@class, \'title-info-title-text\')]/text()')
@@ -228,14 +215,15 @@ class AvitoRuSpider(scrapy.Spider):
         # plot_size
         # ad_loader.add_value('plot_size', self.get_total_square(response))
         ad_loader.add_value('cost', self.get_cost(response))
-        ad_loader.add_value('phone', self.get_phone(response))
+
         ad_loader.add_value('address', self.get_address(response))
         ad_loader.add_value('description', self.get_description(response))
         ad_loader.add_value('category', self.get_category(response))
         ad_loader.add_value('floor_count', self.get_floor_count(response))
         ad_loader.add_value('contact_name', self.get_contact_name(response))
         ad_loader.add_value('image_list', self.get_image_list(response))
-        return ad_loader.load_item()
+        url = response.url.replace('www.', 'm.')
+        yield response.follow(url, callback=self.parse_mobile, meta={'ad_loader': ad_loader})
 
     def parse(self, response):
         last_reached = False
--- run.py
+++ run.py
@@ -21,15 +21,15 @@ scrappers = [
 #os.environ["http_proxy"] = "http://localhost:8888"
 #os.environ["http_proxy"] = "lum-customer-hl_d97be066-zone-zone1-ip-66.78.37.237:ysjouo4rfjja@zproxy.lum-superproxy.io:22225"
 
-with Xvfb() as xvfb:
-    sys.path.append(os.getcwd())
-    settings = get_project_settings()
-    process = CrawlerProcess(settings)
-    Logger.log("INFO", "Starting the realty scrappers.")
-    for scrapper in scrappers:
-        process.crawl(scrapper)
-    process.start()
-    Logger.log("INFO", "Stopping the realty scrappers")
-    Logger.log("INFO", "Calling for the clean")
-    r = requests.delete('http://realty.zmservice.ru/api/remove_old.json')
-    print(r.content)
+#with Xvfb() as xvfb:
+sys.path.append(os.getcwd())
+settings = get_project_settings()
+process = CrawlerProcess(settings)
+Logger.log("INFO", "Starting the realty scrappers.")
+for scrapper in scrappers:
+    process.crawl(scrapper)
+process.start()
+Logger.log("INFO", "Stopping the realty scrappers")
+Logger.log("INFO", "Calling for the clean")
+r = requests.delete('http://realty.zmservice.ru/api/remove_old.json')
+print(r.content)
